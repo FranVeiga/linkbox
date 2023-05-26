8adbb13771ba993c0ef50a2e83eb6d4ed6a192ce
Francisco Veiga <francisco2.veiga@gmail.com>
2023-05-26 13:45:24 -0300

    feat: set up mongodb connection

--- server/app.js
+++ server/app.js
@@ -2,8 +2,12 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var dotenv = require("dotenv");
+const { MongoClient } = require("mongodb");
 
 var apiRouter = require("./routes/api");
+
+dotenv.config();
 
 var app = express();
 
@@ -11,7 +15,22 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, "public")));
+
+// Test initial connection to MongoDB database
+const MONGODB_USER = process.env.MONGODB_USER;
+const MONGODB_PASS = process.env.MONGODB_PASS;
+const uri = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASS}@linkbox.mnmvy6k.mongodb.net/?retryWrites=true&w=majority`;
+async function dbConnectionTest() {
+    const dbClient = new MongoClient(uri);
+    await dbClient.connect(uri);
+    await dbClient.close();
+}
+dbConnectionTest()
+    .then(() => console.log("Successful connection to database."))
+    .catch((e) => {
+        console.dir(e);
+        process.exit(1);
+    });
 
 app.use("/api/v1", apiRouter);
 
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,23 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const router = express.Router();
+const dotenv = require("dotenv");
+dotenv.config();
 
-router.get("/", function(req, res, next) {
-  res.send("Hola que tal");
-  next();
+const MONGODB_USER = process.env.MONGODB_USER;
+const MONGODB_PASS = process.env.MONGODB_PASS;
+const uri = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASS}@linkbox.mnmvy6k.mongodb.net/?retryWrites=true&w=majority`;
+
+const dbClient = new MongoClient(uri);
+dbClient.connect({
+    server: {
+        auto_reconnect: true,
+    },
+});
+
+router.get("/", async function(req, res, next) {
+    const person = await dbClient.db("accounts").collection("users").findOne();
+    res.json(person);
 });
 
 module.exports = router;
