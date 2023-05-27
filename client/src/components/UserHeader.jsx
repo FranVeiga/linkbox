574d5353f086fc4f60d9e3db6909f4c25367c044
Francisco Veiga <francisco2.veiga@gmail.com>
2023-05-27 13:05:20 -0300

    Added tailwind

--- client/src/components/UserHeader.jsx
+++ client/src/components/UserHeader.jsx
@@ -2,8 +2,8 @@ export default function UserHeader(props) {
     const { name } = props;
     return (
         <>
-            <header>
-                <h1 className="title">{name}</h1>
+            <header className="">
+                <h1 className="">{name}</h1>
             </header>
         </>
     );
